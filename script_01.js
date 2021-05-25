6c427e21439816304a1f7cb0c65084a1062ca322
Peter <schmidt.multimedia@gmail.com>
2021-05-25 11:10:48 +0200

    For-Schleifen

--- script_01.js
+++ script_01.js
@@ -2,18 +2,48 @@
 
 /*** 1.0 Arrays */
 
-let arr;
+// let arr;
 // arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,6,7,12];
-arr = [true,false];
-arr = ["Ich","bin","Max","Mütze"];
+// arr = []; // Literal
+// arr = [2,6,7,12];
+// arr = [true,false];
+// arr = ["Ich","bin","Max","Mütze"];
 
-ausgabe(arr);
-ausgabe(arr.length);
-ausgabe(arr[0]); // Index, begint IMMER mit 0
-ausgabe(arr[1]);
+// ausgabe(arr);
+// ausgabe(arr.length);
+// ausgabe(arr[0]); // Index, begint IMMER mit 0
+// ausgabe(arr[1]);
 
+/* 1a. Schleifen (for-schleife) */
+
+/* For -Schleife als Wiederholungs-Struktur */
+// for (let i = 0; i < 5; i++) {
+//     ausgabe(i); 
+// }
+
+/* For-Schleife für Array-Index (Iteration)*/
+let arr = ["Ich","bin","der","coole","Max","Mütze"];
+for (let i = 0; i < arr.length; i++) {
+    ausgabe(arr[i]); 
+}
+
+
+/*** 02a. Funktionalität mit Array  */
+// Kritik | Überlegung
+// word1,word2, .... word100  :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation  sematisches Problem --> numerisches Problem
+//ausgabe(getSentenceArr(["Ich","bin","Max","Mütze"])); // Test
+function getSentenceArr(arr) {
+    let gap = " ";
+    let punct = ".";
+    let str =   arr[0] + gap +
+                arr[1] + gap +
+                arr[2] + gap +
+                arr[3] + 
+                punct;
+    return str;
+}
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
